449e08ee82b020d8f50b1d4ac32aa713891eadc5
Yashini13 <142714954+Yashini13@users.noreply.github.com>
2024-09-23 20:10:03 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://mern-stack-project-1-8vlj.onrender.com",
     credentials: true
 }
 
